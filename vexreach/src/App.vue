10ffa6b0e9f080b939565f68f0fa97b08c4cda3d
V1INTIATIVE <shreyanshlakra724@gmail.com>
2026-07-18 13:11:10 +0530

    Initial commit

---
+++ vexreach/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+
+const welcomeMessage = ref('Welcome to the VexReach ')
+</script>
+
+<template>
+  <main class="main-content">
+    <h2>{{ welcomeMessage }}</h2>
+    
+
+    
+    </main>
+</template>
+
+<style scoped>
+/* Scoped styles will only apply to this component */
+.main-content {
+  padding: 2rem;
+  font-family: sans-serif;
+}
+</style>
